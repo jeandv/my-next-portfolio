9ac38f020f07a9eda7dd09e16c001b71e9017340
Jean Rondon <jeanrdev@gmail.com>
2023-01-23 03:43:05 -0400

    confirm change in dev

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ const HomePage = () => {
   return (
     <div>
       <h1 className='text-5xl'>Welcome to my-next-portfolio</h1>
-      <p className='text-5xl'>esto es la configuración inicial :3</p>
+      <p className='text-5xl'>esta es la branch "dev" donde desarrollare mi portfolio!</p>
     </div>
   )
 }
