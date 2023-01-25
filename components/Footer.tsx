847981867cd02bcc017acc7185bfc3941f0f5333
Jean Rondon <jeanrdev@gmail.com>
2023-01-25 03:32:27 -0400

    Edit and center the footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className='flex flex-col justify-center items-start max-w-2xl mx-auto w-full mb-8'>
       <hr className='w-full border-1 border-gray-200 dark:border-gray-800 mb-8' />
 
-      <div className='w-full max-w-2xl grid grid-cols-1 gap-4 pb-16 sm:grid-cols-3'>
+      <div className='w-full max-w-2xl grid place-items-center grid-cols-1 gap-4 pb-16 sm:grid-cols-3'>
         <div className='flex flex-col space-y-4'>
 
           <Link
