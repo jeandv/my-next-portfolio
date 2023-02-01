efdf2d43444370739052f17ab08f4a5f77160a0e
Jean Rondon <jeanrdev@gmail.com>
2023-02-01 06:23:40 -0400

    adding titles, subtitles and more content to my home section.

--- components/HomeSection.tsx
+++ components/HomeSection.tsx
@@ -59,15 +59,23 @@ const HomeSection = () => {
       <CurrentFavTech />
 
       <motion.div
-        className='flex flex-col-reverse lg:flex-row items-start'
+        className='flex flex-col items-start'
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 0.3 }}
       >
 
+        <h2 className='font-bold text-2xl md:text-2xl tracking-tight mb-4 text-black dark:text-white'>
+          About me
+        </h2>
+
         <p className='text-gray-600 dark:text-gray-400 mb-6'>
           I'm a passionate dev with +2 year of work experience in JavaScript/TypeScript, I'm a lover of page design or other things and whenever I can I try to learn new technologies since I discovered programming at 16 years old. I also use in web, mobile and desktop development Next.js, Angular, Ionic and Electron.js, to add I also have experience working in Backend with Node and in the UX design part with Figma, always implementing good practices in prototyping and coding, clean architecture, SOLID principles, pixel perfect and agile methodologies.
         </p>
+
+        <h2 className='font-bold text-2xl md:text-2xl tracking-tight mb-4 text-black dark:text-white'>
+          Fav projects
+        </h2>
 
       </motion.div>
 
