ce3d7ce6b0abfbcaea64e5ed3d24e4f1937b6c5d
Jean Rondon <jeanrdev@gmail.com>
2023-02-02 17:45:25 -0400

    Delete circles gradients

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -2,17 +2,17 @@
 
 import '../../styles/globals.css';
 
-import { motion } from 'framer-motion';
+// import { motion } from 'framer-motion';
 import { Providers } from '../providers';
 import { Header, Footer } from '@/components/ui';
 
-const animation = {
-  hide: { opacity: 0, y: -20 },
-  show: {
-    y: 0,
-    opacity: 1,
-  },
-};
+// const animation = {
+//   hide: { opacity: 0, y: -20 },
+//   show: {
+//     y: 0,
+//     opacity: 1,
+//   },
+// };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
 
@@ -21,12 +21,12 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       <body className='bg-gray-50 dark:bg-gray-900'>
         <Providers>
 
-          <motion.div
+          {/* <motion.div
             className='darkBackgroundCirclesColors'
             initial={animation.hide}
             animate={animation.show}
             transition={{ delay: 0.9 }}
-          ></motion.div>
+          ></motion.div> */}
 
           <Header />
 
