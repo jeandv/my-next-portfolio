8cfa4abd9440cf4b18bb761b230b37cb11859969
Jean Rondon <jeanrdev@gmail.com>
2023-02-01 20:54:36 -0400

    creating AboutMe.

---
+++ components/AboutMe.tsx
@@ -0,0 +1,18 @@
+
+const AboutMe = () => {
+  return (
+    <div className='mb-8'>
+
+      <h2 className='font-bold text-2xl md:text-2xl tracking-tight mb-5 text-black dark:text-white'>
+        About me.
+      </h2>
+
+      <p className='text-gray-600 dark:text-gray-400'>
+        I'm a passionate dev with +2 year of work experience in JavaScript/TypeScript, I'm a lover of page design or other things and whenever I can I try to learn new technologies since I discovered programming at 16 years old. I also use in web, mobile and desktop development Next.js, Angular, Ionic and Electron.js, to add I also have experience working in Backend with Node and in the UX design part with Figma, always implementing good practices in prototyping and coding, clean architecture, SOLID principles, pixel perfect and agile methodologies.
+      </p>
+
+    </div>
+  )
+}
+
+export default AboutMe;
--- components/HomeSection.tsx
+++ components/HomeSection.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 import CurrentFavTech from './CurrentFavTech';
+import AboutMe from './AboutMe';
 import FavProjects from './FavProjects';
 
 const HomeSection = () => {
@@ -66,17 +67,7 @@ const HomeSection = () => {
         transition={{ delay: 0.3 }}
       >
 
-        <div className='mb-8'>
-
-          <h2 className='font-bold text-2xl md:text-2xl tracking-tight mb-5 text-black dark:text-white'>
-            About me.
-          </h2>
-
-          <p className='text-gray-600 dark:text-gray-400'>
-            I'm a passionate dev with +2 year of work experience in JavaScript/TypeScript, I'm a lover of page design or other things and whenever I can I try to learn new technologies since I discovered programming at 16 years old. I also use in web, mobile and desktop development Next.js, Angular, Ionic and Electron.js, to add I also have experience working in Backend with Node and in the UX design part with Figma, always implementing good practices in prototyping and coding, clean architecture, SOLID principles, pixel perfect and agile methodologies.
-          </p>
-
-        </div>
+        <AboutMe />
 
         <FavProjects />
 
