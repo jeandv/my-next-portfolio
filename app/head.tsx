47a0950c057dbbf6d6d9fbffb37b0321042b496a
Jean Rondon <jeanrdev@gmail.com>
2023-02-03 00:03:20 -0400

    using usePathname() to get the pathname and modify it for display and use in the SEO

--- app/head.tsx
+++ app/head.tsx
@@ -1,13 +1,15 @@
-// import Head from 'next/head';
-// import { useRouter } from 'next/router';
+'use client'
+
+import { usePathname } from 'next/navigation';
 
 const Head = () => {
 
-  // Nota: usar useRouter para obtener las rutas donde este el user para los meta canonical, etc...
-  // const { asPath } = useRouter();
+  const pathname = usePathname() as string;
+
+  const titleFixedToShow = pathname.slice(1).charAt(0).toUpperCase() + pathname.slice(2);
 
   const meta = {
-    title: 'Jean Rondón · Portfolio',
+    title: `Jean Rondón · ${pathname === '/' ? 'Portfolio' : titleFixedToShow}`,
     description: `Hi! My name is Jean, I'm from Venezuela and I have +1 year of work experience in JavaScript/TypeScript and React, I'm a lover of page design or other things and whenever I can I try to learn new technologies since I discovered programming at 16. I love what I do and I help others with what I can, I also use in web, mobile and desktop development Next.js, Angular, Ionic and Electron implementing good practices, clean architecture, pixel perfect and agile methodologies.`,
     keywords: 'Jean Rondon, Jeandv, Jean Rondon portafolio, Jeandv portafolio, Jeandv portfolio, Jean Rondon portfolio, Jeandv github, Jean Rondon github, Jeandv LinkedIn, Jean Rondon LinkedIn, Web, Desarrollo web, Programador web, Diseño web, Paginas web, Aplicaciones web, Aplicaciones móviles, Aplicaciones escritorio, Desarrollo frontend, Programador frontend, HTML, CSS, Javascript, Typescript, React, Angular.',
     image: '../public/banner-portfolio.png',
@@ -20,8 +22,8 @@ const Head = () => {
       <meta name='robots' content='follow, index' />
       <meta content={meta.description} name='description' />
       <meta name='keywords' content={meta.keywords} />
-      <meta property='og:url' content='https://jeandv.vercel.app' />
-      <link rel='canonical' href='https://jeandv.vercel.app' />
+      <meta property='og:url' content={`https://jeandv.vercel.app${pathname}`} />
+      <link rel='canonical' href={`https://jeandv.vercel.app${pathname}`} />
       <link rel='me' href='mailto:jeanrdev@gmail.com' />
       <meta property='og:type' content={meta.type} />
       <meta property='og:site_name' content='Jean Rondón' />
