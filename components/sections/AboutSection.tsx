13136902fe2890766c7f66bda853001eafc18915
jean <jeanrdev@gmail.com>
2024-03-06 03:40:54 -0400

    Update AboutSection.tsx

--- components/sections/AboutSection.tsx
+++ components/sections/AboutSection.tsx
@@ -66,20 +66,6 @@ const AboutSection = () => {
 
         <ProcessWork />
 
-        <AnimationContainer customClassName='w-full flex flex-col gap-5'>
-
-          <h2 className='font-bold text-2xl md:text-2xl tracking-tight mb-2 text-white text-start'>Interests & Goals</h2>
-
-          <p className='text-base text-gray-400'>
-            I am interested in learning Backend with other language like Java, Go or with Python. I also want to know how to make a video game with Unity or other and I am very interested in being a content creator whether programming or something else.
-          </p>
-
-          <p className='text-base text-gray-400'>
-            For now I'm learning how to make applications with real time communication such as a chat using Socket.IO, I'm also starting to learn the way to be Devops using Docker, Kubernetes, AWS or others.
-          </p>
-
-        </AnimationContainer>
-
       </div>
 
     </SectionContainer>
