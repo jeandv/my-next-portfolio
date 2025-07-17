fdbe334d7f614d241cdc485c3ce2643a91f1c908
Jean Rondón <jeanrdev@gmail.com>
2025-07-17 00:52:48 -0400

    Update mySkills.ts

--- components/utils/mySkills.ts
+++ components/utils/mySkills.ts
@@ -1,22 +1,22 @@
 export const skills = [
   {
-    title: 'Languages',
-    techs: ['JavaScript', 'TypeScript', 'C++']
-  },
-  {
-    title: 'JS Frameworks and libraries',
-    techs: ['React.js', 'Next.js', 'Angular', 'Ionic', 'Electron.js', 'jQuery', 'Redux', 'TanStack Query', 'Zustand', 'Jest', 'Storybook']
-  },
-  {
-    title: 'Preprocessors and styles component libraries',
-    techs: ['Pug', 'Sass', 'Tailwind', 'Framer Motion']
+    title: 'Frontend stack',
+    techs: ['JavaScript', 'TypeScript', 'jQuery', 'React.js', 'Next.js', 'Angular', 'Ionic', 'Electron.js', 'Redux', 'TanStack Query', 'Zustand', 'Jest', 'Storybook', 'Pug', 'Sass', 'Tailwind', 'Framer Motion']
   },
   {
     title: 'Backend stack',
-    techs: ['noSQL', 'Node.js', 'Express.js', 'MongoDB', 'Apollo - Graphql', 'Firebase', 'Sanity CMS']
+    techs: ['noSQL', 'Node.js', 'Express.js', 'MongoDB', 'Apollo - Graphql', 'Firebase', 'Sanity CMS', 'AWS', 'Docker']
+  },
+  {
+    title: 'Electronics and robotic',
+    techs: ['C++', 'Arduino']
   },
   {
     title: 'Softwares and tools',
     techs: ['Git', 'Github', 'Bitbucket', 'Postman', 'Figma', 'Photoshop', 'WordPress', 'Elementor']
   },
+  {
+    title: 'Practices',
+    techs: ['Agile', 'Clean Architecture', 'SOLID', 'Pixel perfect', 'Gitflow', 'Prototyping', 'Microfrontends']
+  }
 ];
