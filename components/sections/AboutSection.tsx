bd0f0be360d440122fd95c524087cdef14d3e7b9
Jean Rondón <jeanrdev@gmail.com>
2025-07-18 17:21:58 -0400

    Update AboutSection.tsx

--- components/sections/AboutSection.tsx
+++ components/sections/AboutSection.tsx
@@ -15,6 +15,20 @@ const AboutSection = () => {
           <p className='text-base text-gray-400'>
             Developer with +{calculateDuration('2022-01-01', false)} years of JavaScript experience and a constant interest in learning new technologies. I have solid experience in Frontend development for web, mobile, and desktop applications, using technologies and frameworks like JavaScript, TypeScript, React, Next.js, Angular, Electron.js, and Ionic. Additionally, I have skills in Backend development with Node.js, Express, and MongoDB (NoSQL). I consistently follow best practices in prototyping, coding, clean architecture, SOLID principles, pixel-perfect design, GitFlow, and agile methodologies. I am currently exploring to be DevOps, by doing projects with tools and technologies such as (AWS, CI/CD, Docker, Kubernetes, Jenkins, Terraform)
           </p>
+          <p className='text-base text-gray-400 flex gap-4 justify-center items-start'>
+            <a className='text-gray-400 hover:text-white inline-flex items-center gap-1' href="https://rxresu.me/jeandv/cv-jean-rondon" target="_blank">
+              CV
+              <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right" aria-hidden="true"><path d="M7 7h10v10"></path><path d="M7 17 17 7"></path></svg>
+            </a>
+            <a className='text-gray-400 hover:text-white inline-flex items-center gap-1' href="https://www.linkedin.com/in/jeanrondon" target="_blank">
+              LinkedIn
+              <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right" aria-hidden="true"><path d="M7 7h10v10"></path><path d="M7 17 17 7"></path></svg>
+            </a>
+            <a className='text-gray-400 hover:text-white inline-flex items-center gap-1' href="https://github.com/jeandv" target="_blank">
+              GitHub
+              <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right" aria-hidden="true"><path d="M7 7h10v10"></path><path d="M7 17 17 7"></path></svg>
+            </a>
+          </p>
         </AnimationContainer>
         <CurrentTimeLineExp />
         <AnimationContainer customClassName='w-full flex flex-col gap-5 mb-8'>
